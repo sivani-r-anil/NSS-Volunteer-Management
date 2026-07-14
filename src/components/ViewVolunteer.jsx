147b760e3b3c1006f52c41c6fd0bd198738a2aa8
sivani-r-anil <sivaniranil28@gmail.com>
2026-07-15 00:47:57 +0530

    Navigation is added

--- src/components/ViewVolunteer.jsx
+++ src/components/ViewVolunteer.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import NavigationBar from './NavigationBar'
 
 const ViewVolunteer = () => {
 
@@ -30,7 +31,7 @@ const ViewVolunteer = () => {
 
     return (
         <div>
-
+            <NavigationBar />
             <div className="container mt-4">
                 <div className="row justify-content-center">
                     <div className="col col-12 col-md-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -39,57 +40,57 @@ const ViewVolunteer = () => {
                             <h2 style={{ color: "#2E7D32", textAlign: "center", fontFamily: "Poppins" }}>
                                 🤝 View Volunteers</h2>
 
-                                <table class="table table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">Volunteer ID</th>
-                                            <th scope="col">Full Name</th>
-                                            <th scope="col">Email</th>
-                                            <th scope="col">Phone</th>
-                                            <th scope="col">Date of Birth</th>
-                                            <th scope="col">Gender</th>
-                                            <th scope="col">Blood Group</th>
-                                            <th scope="col">Department</th>
-                                            <th scope="col">Year of Study</th>
-                                            <th scope="col">Camp Name</th>
-                                            <th scope="col">Hours Completed</th>
-                                            <th scope="col">Address</th>
-                                            <th scope="col">Unit Number</th>
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Volunteer ID</th>
+                                        <th scope="col">Full Name</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Phone</th>
+                                        <th scope="col">Date of Birth</th>
+                                        <th scope="col">Gender</th>
+                                        <th scope="col">Blood Group</th>
+                                        <th scope="col">Department</th>
+                                        <th scope="col">Year of Study</th>
+                                        <th scope="col">Camp Name</th>
+                                        <th scope="col">Hours Completed</th>
+                                        <th scope="col">Address</th>
+                                        <th scope="col">Unit Number</th>
 
 
 
-                                        </tr>
-                                    </thead>
-                                    <tbody>
+                                    </tr>
+                                </thead>
+                                <tbody>
 
-                                        {data.map(
+                                    {data.map(
 
-                                            (data, index) => {
-                                                return (
-                                                    <tr>
-                                                        <td>{data.id}</td>
-                                                        <td>{data.name}</td>
-                                                        <td>{data.email}</td>
-                                                        <td>{data.phone}</td>
-                                                        <td>{data.dob}</td>
-                                                        <td>{data.gender}</td>
-                                                        <td>{data.bloodGroup}</td>
-                                                        <td>{data.dept}</td>
-                                                        <td>{data.year}</td>
-                                                        <td>{data.campName}</td>
-                                                        <td>{data.hours}</td>
-                                                        <td>{data.address}</td>
-                                                        <td>{data.unitNo}</td>
+                                        (data, index) => {
+                                            return (
+                                                <tr>
+                                                    <td>{data.id}</td>
+                                                    <td>{data.name}</td>
+                                                    <td>{data.email}</td>
+                                                    <td>{data.phone}</td>
+                                                    <td>{data.dob}</td>
+                                                    <td>{data.gender}</td>
+                                                    <td>{data.bloodGroup}</td>
+                                                    <td>{data.dept}</td>
+                                                    <td>{data.year}</td>
+                                                    <td>{data.campName}</td>
+                                                    <td>{data.hours}</td>
+                                                    <td>{data.address}</td>
+                                                    <td>{data.unitNo}</td>
 
 
-                                                    </tr>
-                                                )
-                                            }
+                                                </tr>
+                                            )
+                                        }
 
-                                        )}
+                                    )}
 
-                                    </tbody>
-                                </table>
+                                </tbody>
+                            </table>
 
 
                         </div>
